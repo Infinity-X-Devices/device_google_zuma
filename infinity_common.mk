347de12342c751f751a346d9f4a01f70d0080285
Libra420T <ryantf420@gmail.com>
2025-11-03 10:39:25 -0500

    Zuma: Remove PixelParts

--- infinity_common.mk
+++ infinity_common.mk
@@ -41,9 +41,6 @@ PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 PRODUCT_PACKAGES += \
     GoogleParts
 
-# PixelParts <Evolution-X>
-include device/google/zuma/packages/apps/PixelParts/device.mk
-
 # SCUDO    
 TARGET_USES_SCUDO := true
 
